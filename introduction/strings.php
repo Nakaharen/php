82765f35008c326c7f098b09db4ca791702f81bd
Karen Nakahara <karenakahara@gmail.com>
2021-08-05 15:46:08 -0300

    strings

--- introduction/strings.php
+++ introduction/strings.php
@@ -3,4 +3,13 @@
 $idade = 27;
 echo "Olá! Eu tenho $idade anos\n";
 
-// aspas duplas permite concatenar as variáveis e passá-las diretamente no texto.
+/* Para concatenar uma string com uma outra variável usamos o . (ponto)
+Podemos definir uma string com aspas simples(') ou duplas(")
+Para interpretar um valor da variável ou caractere especial dentro da string devemos usar aspas duplas
+***** Aspas duplas permite concatenar as variáveis e passá-las diretamente no texto.
+
+Caracteres especias são:
+Nova linha: \n ou \r
+Tab: \t
+O PHP possui uma constante para indicar uma quebra de linha: PHP_EOL */
+
