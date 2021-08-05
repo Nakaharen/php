48f986d766121da3be54e9ec07ae70519e0e62de
Karen Nakahara <karenakahara@gmail.com>
2021-08-05 17:19:25 -0300

    decisoes

---
+++ introduction/decisoes.php
@@ -0,0 +1,15 @@
+<?php
+
+  $idade = 18;
+
+  echo "Você pode entrar a partir de 18 anos." . PHP_EOL;
+
+  if ($idade >= 18) {
+    echo "Você tem $idade anos." . PHP_EOL;
+    echo 'Pode entrar';
+  }
+
+  // || = or => OU
+
+  // && = and => E
+
