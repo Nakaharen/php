a7cc7de0965f26c7e1559c2487322d1a5eadab46
Karen Nakahara <karenakahara@gmail.com>
2021-08-05 14:54:57 -0300

    tipos de dados

---
+++ introduction/tipos-de-dados.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$salario = 1000.30;
+
+echo gettype($salario);
+// double - número decimal
+
+$texto = "Hello world";
+// string
+
+$verdadeiro = true;
+$falso = false;
+// boolean
+
+/* tipos primitivos - a partir desses tipos é possível criar
+tipos complexos */
+
