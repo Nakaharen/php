cc5e42bba5b93937f2af0e781daf0eee557ec57a
Karen Nakahara <karenakahara@gmail.com>
2021-08-05 14:13:26 -0300

    operacoes

---
+++ introduction/operacoes.php
@@ -0,0 +1,28 @@
+<?php
+
+  $idade = 27;
+  $idadeDaqui10Anos = $idade + 10;
+
+  echo "$idadeDaqui10Anos\n";
+
+/* ------------------------------------------ */
+
+  $idade = 27;
+  $idadeHa5Anos = $idade - 5;
+
+  echo "$idadeHa5Anos\n";
+
+/* ------------------------------------------ */
+
+  $soma = 2 + 2;
+  $subtracao = 2 -2;
+  $multiplicacao = 2 * 2;
+  $divisao = 2/2;
+
+  $doisAoCubo = 2 ** 3;
+
+  echo "$doisAoCubo\n";
+
+  $restoDaDivisao = 10 % 3;
+
+  echo "$restoDaDivisao\n";
