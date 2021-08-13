085f6d9a8b8e4435a999eef22b041e8905f1e860
Karen Nakahara <karenakahara@gmail.com>
2021-08-13 14:29:17 -0300

    separacao de arquivos

--- introduction/strings-complexas.php
+++ introduction/strings-complexas.php
@@ -1,32 +1,6 @@
 <?php
 
-function sacar(array $conta, float $valorASacar)
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar este valor");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-}
-
-function exibeMensagem(string $mensagem)
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function depositar(array $conta, float $valorADepositar)
-{
-  if($valorADepositar > 0) {
-    $conta['saldo'] += $valorADepositar;
-  } else {
-    exibeMensagem("O valor de depósito precisa ser maior que 0.");
-  }
-
-  $conta['saldo'] += $valorADepositar;
-  return $conta;
-}
+require 'funcoes-sc.php';
 
 $contasCorrentes = [
     '123.456.789-10' => [
@@ -42,7 +16,6 @@ $contasCorrentes = [
         'saldo' => 10000
     ]
 ];
-
 
 $contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 50);
 $contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'], 300);
