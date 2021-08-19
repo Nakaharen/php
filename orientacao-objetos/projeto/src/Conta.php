529ec4caeb01578f0bfecbe31c3d310fdb2c67c2
Karen Nakahara <karenakahara@gmail.com>
2021-08-18 21:30:29 -0300

    this

--- orientacao-objetos/projeto/src/Conta.php
+++ orientacao-objetos/projeto/src/Conta.php
@@ -5,4 +5,13 @@ class Conta
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar)
+    {
+        if ($valorASacar > $this->saldo) {
+            echo 'Saldo indisponível';
+        } else {
+            $this->saldo -= $valorASacar;
+        }
+    }
 }
