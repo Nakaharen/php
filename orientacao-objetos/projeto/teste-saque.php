5ff7ddf8050ee03f94693fda8f70374b62d92f8a
Karen Nakahara <karenakahara@gmail.com>
2021-08-27 18:55:18 -0300

    classes filhas

--- orientacao-objetos/projeto/teste-saque.php
+++ orientacao-objetos/projeto/teste-saque.php
@@ -1,19 +1,18 @@
 <?php
 
-use Alura\Banco\Modelo\Conta\Conta;
+use Alura\Banco\Modelo\Conta\ContaPoupanca;
 use Alura\Banco\Modelo\Conta\Titular;
 use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Modelo\Endereco;
 
 require_once 'autoload.php';
 
-$conta = new Conta(
+$conta = new ContaPoupanca(
     new Titular(
         new CPF('123.456.789-10'),
         'Karen Nakahara',
         new Endereco('São José dos Campos', 'bairro teste', 'rua teste', '123')
-    ),
-    2
+    )
 );
 
 $conta->depositar(500);
