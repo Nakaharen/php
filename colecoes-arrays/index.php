d1a2b98ddbc70e3c58090138649488da8c1f6a9b
Karen Nakahara <karenakahara@gmail.com>
2021-09-03 19:30:04 -0300

    refatorando para um metodo

--- colecoes-arrays/index.php
+++ colecoes-arrays/index.php
@@ -1,20 +1,14 @@
 <?php
 
+require 'Calculadora.php';
+
 $notas = [10, 5, 7, 10, 9];
 
-echo "<p>A nota de Português é: $notas[0]</p>";
-echo "<p>A nota de Matemática é: $notas[1]</p>";
-echo "<p>A nota de Geografia é: $notas[2]</p>";
-echo "<p>A nota de Biologia é: $notas[3]</p>";
-echo "<p>A nota de História é: $notas[4]</p>";
+$calculadora = new Calculadora();
+$media = $calculadora->calculaMedia($notas);
 
-$quantidadeNotas = sizeof($notas);
-
-$soma = 0;
-for ($i = 0; $i < $quantidadeNotas; $i ++) {
-  $soma = $soma + $notas [$i];
+if ($media) {
+  echo "A média é: $media";
+} else {
+  echo "Não foi possível calcular a média";
 }
-
-$media = $soma / $quantidadeNotas;
-
-echo "<p>A média é: $media</p>";
