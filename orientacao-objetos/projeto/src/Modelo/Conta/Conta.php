985f6de80c2069c535deadde5f8abe468a4d9a24
Karen Nakahara <karenakahara@gmail.com>
2021-09-22 10:40:16 -0300

    lancando excecao existente

--- orientacao-objetos/projeto/src/Modelo/Conta/Conta.php
+++ orientacao-objetos/projeto/src/Modelo/Conta/Conta.php
@@ -35,8 +35,7 @@ abstract class Conta
     public function depositar(float $valorADepositar): void
     {
         if ($valorADepositar < 0) {
-            echo "Valor precisa ser positivo";
-            return;
+            throw new \InvalidArgumentException();
         }
 
         $this->saldo += $valorADepositar;
