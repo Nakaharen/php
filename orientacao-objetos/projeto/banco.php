fd93563062382c8d3bb6e954c8f240ef4b11da01
Karen Nakahara <karenakahara@gmail.com>
2021-09-23 11:27:08 -0300

    try catch

--- orientacao-objetos/projeto/banco.php
+++ orientacao-objetos/projeto/banco.php
@@ -9,7 +9,12 @@ use Alura\Banco\Modelo\Conta\{Conta, ContaCorrente, ContaPoupanca};
 
 $endereco = new Endereco('São José dos Campos', 'meu bairro', 'minha rua', '31A' );
 $karen = new Titular(new CPF('123.456.789-10'), 'Karen Nakahara', $endereco);
-$primeiraConta = new ContaCorrente($karen);
+try {
+    $primeiraConta = new ContaCorrente($karen);
+} catch (Error $e) {
+    echo "Erro pego:" . $e->getMessage();
+}
+
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300);
 
